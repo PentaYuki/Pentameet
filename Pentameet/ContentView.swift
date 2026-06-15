da4b4698a805dc0d5596b05c4e0e9b59420056e3
KhoaLD <gooleseswsq1@gmail.com>
2026-06-15 20:27:25 +0700

    Initial Commit

---
+++ Pentameet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pentameet
+//
+//  Created by Lê Đăng Khoa  on 2026/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pentameet/PentameetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PentameetApp.swift
+//  Pentameet
+//
+//  Created by Lê Đăng Khoa  on 2026/06/15.
+//
+
+import SwiftUI
+
+@main
+struct PentameetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
